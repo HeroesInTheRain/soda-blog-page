d501226900a8700feb9b44fc12984f3a2bff2149
Shary Ibarra <shariany97@hotmail.com>
2022-01-19 12:11:39 -0700

    First push

---
+++ src/Pages/Timeline.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from '../Components/Post'
+
+const Timeline = () => {
+    return (
+        <div>
+            <h1>Timeline</h1>
+            <Post />
+        </div>
+    )
+}
+
+export default Timeline
